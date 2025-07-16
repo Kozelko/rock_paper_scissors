66dcc2547adc9031791ada596399ef3c6d6ec7af
Peter Brandajský <71568839+Kozelko@users.noreply.github.com>
2025-07-16 22:40:11 +0200

    Add function named gedComputerChoice

--- myScript.js
+++ myScript.js
@@ -1,5 +1,16 @@
 function getComputerChoice() {
-
+    random = Math.floor(Math.random() * 3)
+    value = "";
+    if (random == 0) {
+        value = "rock"
+    }
+    else if (random == 1) {
+        value = "paper"
+    }
+    else {
+        value = "scissors"
+    }
+    return value;
 }
 
-getComputerChoice
+console.log(getComputerChoice())
