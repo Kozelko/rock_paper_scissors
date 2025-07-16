5c466b80ca6f5c6b76aa08de1a751a6c9cd10fe2
Peter Brandajský <71568839+Kozelko@users.noreply.github.com>
2025-07-16 22:49:56 +0200

    Add function getHumanChoice()

--- myScript.js
+++ myScript.js
@@ -13,4 +13,33 @@ function getComputerChoice() {
     return value;
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    console.log("0 = rock\n1 = paper\n2 = scissors")
+    choice = prompt()
+    console.log(choice)
+    if (choice == "0") {
+        choice = "rock"
+    }
+    else if (choice == "1") {
+        choice = "paper"
+    }
+    else if (choice == "2") {
+        choice = "scissors"
+    }
+    else {
+        console.log("Invalit input")
+    }
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
+humanScore = 0
+computerScore = 0
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
